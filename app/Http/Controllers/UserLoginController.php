f97884fdedaa0425faeb7805f1950dd538da6dc3
Gokul Srinivas <gokusrinivas@gmail.com>
2016-01-08 20:14:14 +0530

    Add UserLoginController
    UserLoginController checks whether the user exists, validates with LDAP
    or IMAP and sends the appropriate response to the client.

---
+++ app/Http/Controllers/UserLoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Helpers\JSONResponse;
+use App\Helpers\LDAPAuth;
+use App\Helpers\IMAPAuth;
+use App\User;
+
+class UserLoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user_roll = $request->input('user_roll');
+    	$user_pass = $request->input('user_pass');
+
+    	$ldap_auth = LDAPAuth::auth($user_roll,$user_pass);
+    	$imap_auth = IMAPAuth::auth($user_roll,$user_pass);
+
+    	if($ldap_auth || $imap_auth)
+    	{
+    		$user = User::where('user_roll','=',$user_roll)
+						->first();
+
+            if($user == NULL)
+            {
+                return JSONResponse::response(400);
+            }
+			$generated_secret = sha1($user->user_roll."Pragyan16Rocks");
+
+			$user->user_secret = $generated_secret;
+			$user->save();
+
+			return JSONResponse::response(200,$generated_secret);
+    	}
+    	else
+    	{
+    		return JSONResponse::response(401);
+    	}
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,11 @@
 
 $api = app('Dingo\Api\Routing\Router');
 
+// The open and unauth routes
 $api->version('v1', function ($api) {
+	$api->post('/login', 'App\Http\Controllers\UserLoginController@login');
+});
+// The 
+$api->version('v1', ['middleware' => 'userauth'], function ($api) {
 	$api->get('/test', 'App\Http\Controllers\GcmController@register');
 });
