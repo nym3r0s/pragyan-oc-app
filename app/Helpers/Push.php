23c5436223ef735ce79661381bff0e04da5b7fc9
Gokul Srinivas <gokusrinivas@gmail.com>
2016-01-16 00:09:51 +0530

    Add methods to ease mobile push
    Add method `jsonEncode` to encode a json with the specific format
    including `type` and `message` so that the android client can parse it
    for a specific event.
    
    Add method `sendMany` so that we can send messages to a specific user
    or many users for easy pushes from a controller.

--- app/Helpers/Push.php
+++ app/Helpers/Push.php
@@ -9,6 +9,8 @@ use Log;
 
 use PushNotification;
 use App\User;
+use Exception;
+use stdClass;
 
 class Push
 {
@@ -24,11 +26,49 @@ class Push
 
 		$user_gcmid = $user->user_gcmid;
 
+		try 
+		{
+			PushNotification::app('appNameAndroid')
+			                ->to($user_gcmid)
+			                ->send($message);
+		}
+		catch(Exception $e)
+		{
+			Log::info("Push failed for ".$user_roll." with Message\n".json_encode($message));
+		}
 
+	}
 
-		PushNotification::app('appNameAndroid')
-		                ->to($user_gcmid)
-		                ->send('Hello RB. Life la Eppdi pogudhu');
+	public static function jsonEncode($type="default",$data=NULL)
+	{
+		$jsonObject = new stdClass();
+		$jsonObject->type = $type;
+		$jsonObject->message = $data;
+
+		return json_encode($jsonObject); 
+	}
+
+	public static function sendMany($rollnums = NULL,$message = NULL)
+	{
+		// If rollnum is not specified, return
+		if($rollnums == NULL)
+		{
+			return;
+		}
+
+		// Check if only one roll number is sent.
+		if(!is_array($rollnums))
+		{
+			Push::notify($rollnums, $message);
+		}
+		// Loop through the roll numbers and send the message to each and every one of them
+		else 
+		{
+			foreach ($rollnums as $rollnum)
+			{
+				Push::notify($rollnum, $message);
+			}
+		}
 	}
 }
 ?>
